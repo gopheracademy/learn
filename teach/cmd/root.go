995c3fec0c88996d5d7223409638b1b6975d5566
Mark Bates <mark@markbates.com>
2017-02-10 14:51:19 -0500

    you can now pass a file to teach

--- teach/cmd/root.go
+++ teach/cmd/root.go
@@ -1,11 +1,14 @@
 package cmd
 
 import (
+	"bytes"
 	"fmt"
+	"io/ioutil"
 	"log"
 	"net/http"
 	"os"
 	"path/filepath"
+	"strings"
 
 	"github.com/gobuffalo/envy"
 	"github.com/gopheracademy/learn/models"
@@ -17,6 +20,7 @@ import (
 var rootPath = filepath.Join(os.Getenv("GOPATH"), "src", "github.com", "gopheracademy", "learn", "teach", "cmd")
 var modules []*models.Module
 var moduleMap = map[string]*models.Module{}
+var iptFile string
 
 // RootCmd represents the base command when called without any subcommands
 var RootCmd = &cobra.Command{
@@ -25,6 +29,18 @@ var RootCmd = &cobra.Command{
 	// Uncomment the following line if your bare application
 	// has an action associated with it:
 	PreRunE: func(cmd *cobra.Command, args []string) error {
+		if iptFile != "" {
+			b, err := ioutil.ReadFile(iptFile)
+			if err != nil {
+				return err
+			}
+			files := strings.Split(string(bytes.TrimSpace(b)), "\n")
+			for i, f := range files {
+				f = filepath.Join(models.ModulesPath, f, "module.md")
+				files[i] = f
+			}
+			return setupModules(files)
+		}
 		files, err := models.FindModuleFiles()
 		if err != nil {
 			return err
@@ -33,13 +49,19 @@ var RootCmd = &cobra.Command{
 	},
 	Run: func(cmd *cobra.Command, args []string) {
 		mux := mux.NewRouter()
+
 		a := http.StripPrefix("/assets/", http.FileServer(http.Dir(filepath.Join(rootPath, "assets"))))
 		mux.PathPrefix("/assets/").Handler(a)
+
 		ta := http.StripPrefix("/training-assets/", http.FileServer(http.Dir(models.ModulesPath)))
 		mux.PathPrefix("/training-assets/").Handler(ta)
+
 		mux.HandleFunc("/", IndexHandler)
 		mux.HandleFunc("/module/{name}", ModuleHandler)
-		log.Fatal(http.ListenAndServe(envy.Get("PORT", ":8000"), mux))
+
+		port := envy.Get("PORT", ":8000")
+		fmt.Printf("Starting teach on %s\n", port)
+		log.Fatal(http.ListenAndServe(port, mux))
 	},
 }
 
@@ -62,6 +84,10 @@ func setupModules(files []string) error {
 		moduleMap[m.Slug] = m
 	}
 	return nil
+}
+
+func init() {
+	RootCmd.Flags().StringVarP(&iptFile, "file", "f", "", "a file with a list of modules to use.")
 }
 
 // Execute adds all child commands to the root command sets flags appropriately.
