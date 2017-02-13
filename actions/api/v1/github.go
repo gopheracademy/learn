298d4035ded171a20bd0a1d00e30d041b76d8e2b
Mark Bates <mark@markbates.com>
2017-02-13 17:10:25 -0500

    added a whole crap load of logging around github

--- actions/api/v1/github.go
+++ actions/api/v1/github.go
@@ -1,6 +1,7 @@
 package v1
 
 import (
+	"fmt"
 	"strings"
 
 	"github.com/gobuffalo/buffalo"
@@ -14,16 +15,25 @@ import (
 var hmac = hmax.New("X-Hub-Signature", []byte(envy.Get("GITHUB_WEBHOOK_SECRET", "some-secret")))
 
 func GitHubWebhook(c buffalo.Context) error {
+	fmt.Printf("### hmac.Secret -> %+v\n", string(hmac.Secret))
+	c.Logger().Infof("### hmac.Secret -> %+v\n", string(hmac.Secret))
 	req := c.Request()
 	xhs := req.Header.Get(hmac.Header)
+	fmt.Printf("### original github header -> %+v\n", xhs)
+	c.Logger().Infof("### original github header -> %+v\n", xhs)
 	xhs = strings.TrimPrefix(xhs, "sha1=")
+	fmt.Printf("### modified github header -> %+v\n", xhs)
+	c.Logger().Infof("### modified github header -> %+v\n", xhs)
 	req.Header.Set(hmac.Header, xhs)
 	b, err := hmac.VerifyRequest(req)
 	if err != nil {
+		c.Logger().Error(err)
 		return err
 	}
 	if !b {
-		return errors.Errorf("could not verify signature with %s", c.Request().Header.Get(hmac.Header))
+		err = errors.Errorf("could not verify signature with %s", xhs)
+		c.Logger().Error(err)
+		return c.Error(422, err)
 	}
 	go func(l buffalo.Logger) {
 		gc := grift.NewContext("modules")
