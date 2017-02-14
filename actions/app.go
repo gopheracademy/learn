5e42b4a89382f99805ff1a57249064e5412158ce
Mark Bates <mark@markbates.com>
2017-02-13 19:42:41 -0500

    fixed host to use an env var

--- actions/app.go
+++ actions/app.go
@@ -1,21 +1,21 @@
 package actions
 
 import (
-	"os"
+	"fmt"
 	"time"
 
 	"github.com/gobuffalo/buffalo"
 	"github.com/gobuffalo/buffalo/middleware"
+	"github.com/gobuffalo/envy"
 	"github.com/gopheracademy/learn/actions/api/v1"
 	"github.com/gopheracademy/learn/models"
-	"github.com/markbates/going/defaults"
 
 	"github.com/markbates/goth/gothic"
 )
 
 // ENV is used to help switch settings based on where the
 // application is being run. Default is "development".
-var ENV = defaults.String(os.Getenv("GO_ENV"), "development")
+var ENV = envy.Get("GO_ENV", "development")
 var app *buffalo.App
 
 // App is where all routes and middleware for buffalo
@@ -23,9 +23,12 @@ var app *buffalo.App
 // application.
 func App() *buffalo.App {
 	if app == nil {
+
+		host := envy.Get("HOST", fmt.Sprintf("http://127.0.0.1:%s", envy.Get("PORT", "3000")))
 		app = buffalo.Automatic(buffalo.Options{
 			Env:         ENV,
 			SessionName: "_learn_session",
+			Host:        host,
 		})
 
 		app.Use(middleware.PopTransaction(models.DB))
